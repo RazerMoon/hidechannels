847d7705ea4fb5b200928a1b4fe98a444f322aa8
Rasync <gamespeterplelit@gmail.com>
2021-03-28 22:31:59 +0100

    Removed id text input

--- Settings.jsx
+++ Settings.jsx
@@ -4,8 +4,6 @@ const { ButtonItem } = require("powercord/components/settings");
 module.exports = ({ getSetting, updateSetting, settings }) => {
   const settingsApi = powercord.pluginManager.plugins.get("hidechannels").settings;
 
-  const [error, setError] = React.useState(false);
-
   const details = getSetting("details", []);
 
   const Note = ({ id, guild_id, type }) => (
@@ -48,36 +46,8 @@ module.exports = ({ getSetting, updateSetting, settings }) => {
     }
   };
 
-  /*
-  const onChange = (val) => {
-    // Removes whitespace
-    const sanitized = val.replace(/\s/g, "")
-
-    // Tests if string only consists of digits seperate by commas
-    if (sanitized !== "" && !(/^\d+(,\d+)*$/).test(sanitized)) {
-      setError(true)
-      return;
-    }
-
-    error && setError(false)
-
-    updateSetting("idlist", sanitized.split(","))
-  }
-  */
-
   return (
     <div>
-      {/*
-      <TextInput
-        note="List of Text Channel IDs to block, seperated by commas."
-        defaultValue={getSetting("idlist", [""])}
-        onChange={onChange}
-        placeholder={["805984053131870209", "691183062792536104"]}
-        error={error && "Illegal value! Make sure to only use digits and no comma at the end!"}
-      >
-        ID List
-      </TextInput>
-      */}
       <ButtonItem
         note="Log all setting data to console"
         button="Do it"
