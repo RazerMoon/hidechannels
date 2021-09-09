f6b14d99cf5b3fbf5d74fc26f7a8061ba8c4a713
Rasync <gamespeterplelit@gmail.com>
2021-09-09 16:27:13 +0100

    fix set crash

--- utils/hideChannel.js
+++ utils/hideChannel.js
@@ -31,9 +31,4 @@ module.exports = function removeChannel (channel, settings = powercord.pluginMan
   if (!details.some(item => item.id === channel.id)) {
     details.push(channel);
   }
-
-  // eslint-disable-next-line no-warning-comments
-  // TODO: Don't update if nothing changed
-  settings.set('idlist', list);
-  settings.set('details', details);
 };
